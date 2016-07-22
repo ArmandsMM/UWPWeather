75b948d251727230964e049015de65c3105c76a6
Armands <armands.mikanovskis@accenture.com>
2016-07-22 11:26:19 +0300

    one time update on page loaded

--- UWPWeather/MainPage.xaml.cs
+++ UWPWeather/MainPage.xaml.cs
@@ -5,12 +5,14 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -25,6 +27,39 @@ namespace UWPWeather
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string pair = "eth_usd";
+            var position = await LocationManager.GetPosition();
+            RootObject ticker = await OpenWeatherProxy.GetData(pair);
+            resultTextBlock.Text = "Last price: " + ticker.ticker.last.ToString() + " and position is " + position.Coordinate.Latitude;
+
+
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string pair = "eth_usd";
+                var position = await LocationManager.GetPosition();
+                RootObject ticker = await OpenWeatherProxy.GetData(pair);
+                resultTextBlock.Text = "Last price: " + ticker.ticker.last.ToString() + " and position is " + position.Coordinate.Latitude;
+
+                //Schedule update
+                var uri = "https://btc-e.com/api/2/" + pair + "/ticker";
+                var tileContent = new Uri(uri);
+                var requestedInterval = PeriodicUpdateRecurrence.HalfHour;
+                var updater = TileUpdateManager.CreateTileUpdaterForApplication();
+                updater.StartPeriodicUpdate(tileContent, requestedInterval);
+            }
+            catch (Exception)
+            {
+                resultTextBlock.Text = "something went wrong";
+            }
+
         }
     }
 }
---
+++ UWPWeather/OpenWeatherProxy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPWeather
+{
+    class OpenWeatherProxy
+    {
+        public async static Task<RootObject> GetData(string pair)
+        {
+            var http = new HttpClient();
+            var response = await http.GetAsync("https://btc-e.com/api/2/"+pair+"/ticker");
+            var result = await response.Content.ReadAsStringAsync();
+
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+            var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(result));
+
+            var data = (RootObject)serializer.ReadObject(memoryStream);
+
+            return data;
+        }
+    }
+
+    [DataContract]
+    public class Ticker
+    {
+        [DataMember]
+        public double high { get; set; }
+        [DataMember]
+        public double low { get; set; }
+        [DataMember]
+        public double avg { get; set; }
+        [DataMember]
+        public double vol { get; set; }
+        [DataMember]
+        public double vol_cur { get; set; }
+        [DataMember]
+        public double last { get; set; }
+        [DataMember]
+        public double buy { get; set; }
+        [DataMember]
+        public double sell { get; set; }
+        [DataMember]
+        public long updated { get; set; }
+        [DataMember]
+        public long server_time { get; set; }
+    }
+
+    [DataContract]
+    public class RootObject
+    {
+        [DataMember]
+        public Ticker ticker { get; set; }
+    }
+}
